57aae1b9d400e4abb44abadbe55e87c1c8a5239e
mrpvi <ali.parvizi@hotmail.com>
2025-07-03 16:57:52 +0330

    feat: add article preview component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import Hero from "@/components/Hero";
+import ArticlePreview from "@/components/shared/molecules/ArticlePreview";
+
 export default function Home() {
   return (
-    <div className="bg-black text-white">
-      <h1>Hello World</h1>
-    </div>
+    <main>
+        <Hero />
+
+        <section className="my-32">
+          <div className="container mx-auto">
+            <h2 className="text-2xl font-bold">Latest Articles</h2>
+            <div className="flex flex-col gap-4">
+              <ArticlePreview title="Article 1" description="This is a description of article 1" imageUrl="/images/profile.webp" imageAlt="Article 1" />
+              <ArticlePreview title="Article 2" description="This is a description of article 2" imageUrl="/images/profile.webp" imageAlt="Article 2" />
+              <ArticlePreview title="Article 3" description="This is a description of article 3" imageUrl="/images/profile.webp" imageAlt="Article 3" />
+            </div>
+          </div>
+        </section>
+    </main>
   );
 }
--- src/components/shared/organisms/Header/index.tsx
+++ src/components/shared/organisms/Header/index.tsx
@@ -1,13 +1,21 @@
 import Link from "next/link";
 
 export default function Header() {
-  return (
-    <header>
-        <div className="container mx-auto my-6">
-            <Link href="/">
-                <h1 className="text-3xl font-bold font-bitcount">Ali Parvizi</h1>
-            </Link>
-        </div>
-    </header>
-  );
+    return (
+        <header>
+            <div className="container mx-auto my-6">
+                <div className="flex items-center justify-between">
+                    <Link href="/">
+                        <h1 className="text-3xl font-bold font-bitcount">Ali Parvizi</h1>
+                    </Link>
+
+                    <div className="flex items-center gap-4 text-xl text-blue-500 font-bold underline">
+                        <Link href="/about">About</Link>
+                        <Link href="/contact">Contact</Link>
+                    </div>
+                </div>
+
+            </div>
+        </header>
+    );
 }
